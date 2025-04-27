752bd90d8a955770b04eba65be927e2936cf55cf
anshkush02 <ansh.it223020@bppimt.ac.in>
2025-04-27 23:12:39 +0530

    home

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple Animation Trigger on Button Click
+document.querySelector('.start-button').addEventListener('click', () => {
+    alert('Welcome to IT Hustlers! Start your web development journey now!');
+});
